801c266f3f21f01f0d065c43696f4a4812ab41c3
William Kluss <will2k@williams-mbp.gateway.2wire.net>
2014-06-20 22:18:08 -0500

    Put in the drink x app info

--- KlussBusinesssCard/SideProjectViewController.m
+++ KlussBusinesssCard/SideProjectViewController.m
@@ -14,19 +14,14 @@
 
 @implementation SideProjectViewController
 
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
-
-//TODO:Need to test this on a device.
 - (IBAction)tableTopicImageTapped:(UITapGestureRecognizer *)sender {
     NSLog(@"Table Topics image tapped");
     [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://itunes.apple.com/us/app/table-topics-free/id648137044?ls=1&mt=8"]];
+}
+
+- (IBAction)drinkXImageTapped:(UITapGestureRecognizer *)sender {
+    NSLog(@"Not Another Drink App Tapped");
+    [[UIApplication sharedApplication] openURL:[NSURL URLWithString:@"https://bitbucket.org/bklstheman/nada/src"]];
 }
 
 @end
